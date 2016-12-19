eb584e829491faff742d848f2e88df7300b3e4d5
helinjiang <me@helinjiang.com>
2016-12-19 21:23:30 +0800

    file-save

---
+++ common/tools/file-save.js
@@ -0,0 +1,85 @@
+/**
+ * 遍历路径下所有的文件信息，并且保存一份列表在本地
+ *
+ * @author helinjiang
+ *
+ */
+
+var path = require('path');
+var _ = require('lodash');
+var Promise = require('bluebird');
+var fse = Promise.promisifyAll(require('fs-extra'));
+var ProgressBar = require('progress');
+
+var ft = require('./file-tool');
+
+function saveJsonMd5(sourcePath, options) {
+    var fileArr = ft.getAllFiles(sourcePath);
+
+    var map = {},
+        multiNameArr = [];
+
+    if (!options) {
+        options = {
+            noProgressBar: false
+        };
+    }
+
+    // 进度条
+    if (!options.noProgressBar) {
+        var bar = new ProgressBar('filtering [:bar] :current/:total :percent(:etas) , already cost :elapseds ', {
+            complete: '=',
+            incomplete: ' ',
+            width: 20,
+            total: fileArr.length
+        });
+    }
+
+    // 获取 map 和 multiNameArr
+    fileArr.forEach(function (fileItem) {
+        // 注意，如果文件比较多的话，此处每个文件做hash会比较耗时，尤其是文件也相对比较大时，情况更严重
+        // 尝试过遍历包含703张图片，结果耗时189.3s图片，平均一张耗时：269ms;
+        // 10M的视频400ms左右；1.55M图片126ms
+        // 10M的视频290ms左右；1.55M图片50ms
+        var fileName = fileItem.getMd5() + '';
+
+        var arr = map[fileName];
+
+        // 初始化
+        if (!arr) {
+            arr = [];
+        }
+
+        arr.push({
+            p: fileItem.relativePath,
+            s: fileItem.size,
+            t: fileItem.mtime
+        });
+
+        map[fileName] = arr;
+
+        // 判断是否已经有重名的文件了
+        if (arr.length > 1) {
+            multiNameArr.push(fileName);
+        }
+
+        // 进度条
+        if (!options.noProgressBar) {
+            bar.tick();
+        }
+    });
+
+    var result = {
+        basePath: sourcePath,
+        map: map,
+        multiArr: multiNameArr
+    };
+
+    return fse.outputJsonAsync(path.join(sourcePath, 'data.json'), result).then(function () {
+        return result;
+    });
+}
+
+module.exports = {
+    saveJsonMd5: saveJsonMd5
+};
---
+++ test/scripts/tools-file-save.js
@@ -0,0 +1,26 @@
+var fs = require('fs');
+var fse = require('fs-extra');
+var fileSave = require('../../common/tools/file-save');
+var expect = require('chai').expect;
+
+describe('文件保存：根据文件的md5值，保存一份记录在本地', function () {
+    before(function () {
+        fse.removeSync('./test/data/fixtures/save/md5/data.json');
+    });
+
+    after(function () {
+        fse.removeSync('./test/data/fixtures/save/md5/data.json');
+    });
+
+    it('文件保存操作完成', function (done) {
+        fileSave.saveJsonMd5('./test/data/fixtures/save/md5', {noProgressBar: true}).then(function(data){
+            var s1 = fs.readFileSync('./test/data/fixtures/save/md5/data.json','utf8');
+            // console.log(s1.replace(/[\n\r]+/gi, ' '))
+
+            var s2 = fs.readFileSync('./test/data/expected/md5/data.json','utf8');
+
+            expect(s1).to.equal(s2);
+            done();
+        });
+    });
+});
